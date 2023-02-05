576761a1b48fb23b82b497a8c7e03a3554a98965
MaUt89 <53083678+MaUt89@users.noreply.github.com>
2023-02-05 12:34:53 +0100

    Update diagnosis_choco.py

--- Choco/diagnosis_choco.py
+++ Choco/diagnosis_choco.py
@@ -21,25 +21,6 @@ def get_siemens_diagnosis(variable_order_file_path=None):
     return print(result)
 
 
-def get_camera_diagnosis(test_file_path=None, variable_order_file_path=None):
-    try:
-        if variable_order_file_path:
-            result = subprocess.run(["java", "-jar",
-                                     r"C:\Users\mathi\Documents\Studium\Promotion\MF4ChocoSolver-main\ConfigurationChecker\camera_diagnosis.jar",
-                                     test_file_path, variable_order_file_path],
-                                    capture_output=True, text=True, timeout=400)
-        else:
-            result = subprocess.run(["java", "-jar",
-                                     r"C:\Users\mathi\Documents\Studium\Promotion\MF4ChocoSolver-main\ConfigurationChecker\camera_diagnosis.jar",
-                                     test_file_path],
-                                    capture_output=True, text=True, timeout=400)
-
-    except:
-        print('Subprocess did not answer! Continue with another try...')
-
-    return print(result)
-
-
 # get_siemens_diagnosis()
 # get_camera_diagnosis(test_file_path=r"C:\Users\mathi\Documents\Studium\Promotion\MF4ChocoSolver-main\ConfigurationChecker\data\camera_testing_ur.csv")
                      #variable_order_file_path=r"C:\Users\mathi\Documents\Studium\Promotion\MF4ChocoSolver-main\ConfigurationChecker\data\camera_testing_vo.csv")
