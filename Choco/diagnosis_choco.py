e619df42672a2870d918e9c0cf7cf3773a96574e
MaUt89 <53083678+MaUt89@users.noreply.github.com>
2024-08-12 10:16:35 +0200

    Update diagnosis_choco.py
    Changed to REAL-FM-4.sxfm

--- Choco/diagnosis_choco.py
+++ Choco/diagnosis_choco.py
@@ -3,9 +3,11 @@ import subprocess
 
 def get_linux_diagnosis(configuration_file_path):
     try:
+        # change feature model file according to the feature model to be diagnosed
+        # (e.g., linux-2.6.33.3.xml, busybox-1.18.0.xml, ea2468.xml, REAL-FM-4.sxfm)
         result = subprocess.run(["java", "-jar",
                                  r"C:\Users\User\Documents\Studium\Promotion\MF4ChocoSolver-main\LinuxConfiguration\fm_diagnosis.jar",
-                                 r"C:\Users\User\Documents\Studium\Promotion\MF4ChocoSolver-main\LinuxConfiguration\linux-2.6.33.3.xml",
+                                 r"C:\Users\User\Documents\Studium\Promotion\MF4ChocoSolver-main\LinuxConfiguration\REAL-FM-4.sxfm",
                                  configuration_file_path])
     except:
         print('Subprocess did not answer! Continue with another try...')
