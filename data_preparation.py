0c9bf055d79b23ac97d20a769044dd5f04766c5f
MaUt89 <53083678+MaUt89@users.noreply.github.com>
2024-08-12 10:31:40 +0200

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,13 +1,12 @@
 import os
 import csv
 import re
+import numpy as np
 import pandas as pd
 import xml.etree.ElementTree as et
 
-from diagnosis_handling import diagnosis_handling
 
-
-def training_data_from_xml_get(source_xml_files, target_file):
+def training_data_from_xml_get(source_xml_files, target_file, irrelevant_features):
     data_files = []
     for file in os.listdir(source_xml_files):
         if file.endswith(".xml"):
@@ -24,18 +23,16 @@ def training_data_from_xml_get(source_xml_files, target_file):
                 for items in root:
                     for item in items:
                         irrelevant_item = False
-                        with open(
-                                r"C:\Users\mathi\Documents\Studium\Promotion\ConLearn\Learning Data Input\V2_XML\Irrelevant Features_RuleFeatures.txt",
-                                "r") as compare_file:
+                        with open(irrelevant_features) as compare_file:
                             for element in compare_file:
                                 if item.attrib['key'] == element.strip():
                                     irrelevant_item = True
                                     break
                         if not irrelevant_item:
                             headings.append(item.attrib['key'])
-                            headings.append(item.attrib['key'] + "_po")
+                            # headings.append(item.attrib['key'] + "_po")
                             data[item.attrib['key']] = item.attrib['value']
-                            data[item.attrib['key'] + "_po"] = ""
+                            # data[item.attrib['key'] + "_po"] = ""
 
                 file_writer = csv.DictWriter(training_data, headings, delimiter=";")
                 # file_writer.writeheader()
@@ -43,4 +40,4 @@ def training_data_from_xml_get(source_xml_files, target_file):
 
             # print('File %s of %s added.' % (i+1, file_amount))
     # print('Training data ready!\n')
-    return
+    return data
