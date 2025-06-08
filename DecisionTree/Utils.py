6db7dfe8f5db28cc31c81cd95823d7c66a561109
Nhat Minh Hoang <minhsuper24@gmail.com>
2025-06-08 23:08:48 +0200

    trying to fix decision tree

--- DecisionTree/Utils.py
+++ DecisionTree/Utils.py
@@ -686,7 +686,7 @@ def splitData(input_data, output_data):
     chunk_size = int(0.1 * total_data)  # 10% of the total data
 
     # Randomly select the start index for the chunk
-    np.random.seed(420)  # For reproducibility
+    np.random.seed(42)  # For reproducibility
     start_index = np.random.randint(0, total_data - chunk_size)
     end_index = start_index + chunk_size
 
